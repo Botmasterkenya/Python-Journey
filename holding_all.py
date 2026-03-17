129b8e7403241318bee41553d598ca4c541756fb
Botmasterkenya <titotosh613@gmail.com>
2026-03-17 08:50:59 +0300

    Second commit

---
+++ holding_all.py
@@ -0,0 +1,11 @@
+names = []
+
+for i in range(3):
+    name = input("Enter a name: ")
+    names.append(name)
+
+print("\nHere are the names you entered:")
+for n in names:
+    print(n)
+
+print("Done!")
